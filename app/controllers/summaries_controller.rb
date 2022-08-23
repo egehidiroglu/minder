969bdfab9a8ebdca85cd8973979a3e01eedb9dbf
casey tokarchuk <caseytokarchuk@gmail.com>
2022-08-23 12:38:29 -0400

    added routes

---
+++ app/controllers/summaries_controller.rb
@@ -0,0 +1,2 @@
+class SummariesController < ApplicationController
+end
---
+++ test/controllers/summaries_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SummariesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
