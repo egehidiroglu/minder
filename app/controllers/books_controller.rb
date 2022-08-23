9a24405608989155bd785f62612775962fcabbce
casey tokarchuk <caseytokarchuk@gmail.com>
2022-08-23 14:51:34 -0400

    finished routes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,7 @@
 class BooksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :followed_creators, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Validations
+  validates :email, presence: true, format: { with: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/ }
 end
