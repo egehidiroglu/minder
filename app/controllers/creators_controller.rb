77d86d433350b06dcf65a689a7ffcf31aab68bac
Ege Hidiroglu <egehdrgl@gmail.com>
2022-08-25 15:36:51 -0400

    Implemented new and create methods for creator

--- app/controllers/creators_controller.rb
+++ app/controllers/creators_controller.rb
@@ -6,17 +6,17 @@ class CreatorsController < ApplicationController
       when "Music"
         @creators = []
         followed_creators.each do |followed|
-          @creators.push(followed) if followed.creator.content_type == "music"
+          @creators.push(followed) if followed.creator.content_type == "Music"
         end
       when "Movie"
         @creators = []
         followed_creators.each do |followed|
-          @creators.push(followed) if followed.creator.content_type == "movie"
+          @creators.push(followed) if followed.creator.content_type == "Movie"
         end
       when "Book"
         @creators = []
         followed_creators.each do |followed|
-          @creators.push(followed) if followed.creator.content_type == "book"
+          @creators.push(followed) if followed.creator.content_type == "Book"
         end
       end
     else
@@ -25,12 +25,28 @@ class CreatorsController < ApplicationController
   end
 
   def new
-
+    @creator = Creator.new
   end
 
   def create
+    @creator = Creator.new(creator_params)
+    @creator.save
+    followed_creator = FollowedCreator.new
+    followed_creator.creator = @creator
+    followed_creator.user = current_user
+    if followed_creator.save
+      redirect_to my_creators_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def creator_params
+    params.require(:creator).permit(:name, :content_type)
   end
 end
