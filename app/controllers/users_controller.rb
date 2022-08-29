894b79d338f351dea97ee41ec79cafd583016b26
Ege Hidiroglu <egehdrgl@gmail.com>
2022-08-29 17:30:30 -0400

    User can unfav a content from index by clicking on icon

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,4 +41,19 @@ class UsersController < ApplicationController
     redirect_to user_favorites_path
   end
 
+  def custom_unfav
+    favorited = current_user.all_favorited
+    to_unfav = 0
+    favorited.each do |fav|
+      to_unfav = fav if fav.id == params[:id].to_i
+    end
+    current_user.unfavorite(to_unfav)
+    if to_unfav.instance_of?(Movie)
+      redirect_to movies_path
+    elsif to_unfav.instance_of?(Book)
+      redirect_to books_path
+    elsif to_unfav.instance_of?(Album) || to_unfav.instance_of?(Concert)
+      redirect_to musics_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -34,5 +34,6 @@ Rails.application.routes.draw do
   get '/favorite_music/:id', to: 'musics#favorite_music', as: :favorite_music
   get '/favorite_books/:id', to: 'books#favorite_book', as: :favorite_book
   get '/my_favorites', to: 'users#my_favorites', as: :user_favorites
-  get '/favorie_content/:id', to: 'users#unfavorite', as: :unfavorite
+  get '/favorite_content/:id', to: 'users#unfavorite', as: :unfavorite
+  get '/favorite_something/:id', to: 'users#custom_unfav', as: :custom_unfav
 end
